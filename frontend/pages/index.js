6da94d5d6f6466bd28fc7a30bcf078942d30df2d
Ryan Kirkland <ryankirkland.py@gmail.com>
2025-04-17 20:40:28 -0700

    omg its a dash not an underscore

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
     }, 1000); // Increment progress every second
 
     try {
-      const response = await fetch('https://dice-recipe-app.onrender.com/api/generate_recipe', {
+      const response = await fetch('https://dice-recipe-app.onrender.com/api/generate-recipe', {
         method: 'POST',
         body: formData,
       });
